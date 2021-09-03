44f74fee433df01122a334cbe6e98a5b2b261d5d
Wesley McGhee <wesmcghee6372@gmail.com>
2021-09-03 14:17:32 -0500

    added sql queries not done yet

---
+++ server/database.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE langLearn;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    lessonId INT(255) NOT NULL,
+    languageId INT(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE lessons(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    languageId INT(255) NOT NULL
+);
+
+CREATE TABLE languages(
+    id SERIAL PRIMARY KEY,
+    languageName VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE flashCards(
+    id SERIAL PRIMARY KEY,
+    lessonId INT(255) NOT NULL
+);
+
+CREATE TABLE tips(
+    id SERIAL PRIMARY KEY,
+    lessonId INT(255) NOT NULL
+);
+
+
+
